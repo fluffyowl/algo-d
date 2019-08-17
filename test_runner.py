fba003234eacbd7dc5ccd398f021f854e40a94df
nebukuro09 <nebukuro09@gmail.com>
2019-08-17 00:31:40 -0500

    issue#2: Implement test case caching

--- test_runner.py
+++ test_runner.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import os
 import subprocess
 import tempfile
 
+TEST_CASES_DIR = "test_cases_cache"
 OJ_DOWNLOAD_COMMAND = ["oj", "download"]
 OJ_EXECUTE_COMMAND = ["oj", "test", "--tle", "10"]
 COMPILER_COMMAND = ["dmd", "-m64", "-w", "-O", "-release", "-inline", "-J./"]
@@ -12,23 +13,30 @@ class TestRunner:
     def __init__(self, test_path):
         self.test_path = test_path
         self.problem_url = self.get_problem_url(test_path)
+        self.problem_id = self.get_problem_id(self.problem_url)
+        self.test_cases_path = os.path.join(TEST_CASES_DIR, self.problem_id)
         self.required_library_paths = self.get_required_library_paths(test_path)
+
+        if not os.path.exists(TEST_CASES_DIR):
+            os.mkdir(TEST_CASES_DIR)
 
     def run(self):
         with tempfile.TemporaryDirectory(prefix='tmp-') as working_dir:
             source_path = os.path.join(working_dir, "tmp.d")
             binary_path = os.path.join(working_dir, "tmp.out")
-            test_cases_path = os.path.join(working_dir, "testcases/")
             self.generate_source_file(source_path)
             self.compile_source_file(source_path, binary_path)
-            self.download_test_cases(test_cases_path)
-            self.run_tests(binary_path, test_cases_path)
+            self.download_test_cases()
+            self.run_tests(binary_path)
 
     def get_problem_url(self, test_path):
         with open(test_path, 'r') as test_file:
             for line in test_file:
                 if line.startswith("//") and "problem:" in line:
                     return line.rstrip().split()[-1]
+
+    def get_problem_id(self, problem_url):
+        return problem_url.split("?id=")[-1]
 
     def get_required_library_paths(self, test_path):
         required_library_paths = []
@@ -53,12 +61,17 @@ class TestRunner:
         compile_command = COMPILER_COMMAND + ["-of"+binary_path, source_path]
         subprocess.call(compile_command)
 
-    def download_test_cases(self, test_cases_path):
-        download_command = OJ_DOWNLOAD_COMMAND + ["--system", self.problem_url, "-d", test_cases_path]
+    def download_test_cases(self):
+        if os.path.exists(self.test_cases_path) and len(os.listdir(self.test_cases_path)) != 0:
+            print("Test cases already exist. Skip downloading.")
+            return
+        if not os.path.exists(self.test_cases_path):
+            os.mkdir(self.test_cases_path)
+        download_command = OJ_DOWNLOAD_COMMAND + ["--system", self.problem_url, "-d", self.test_cases_path]
         subprocess.call(download_command)
 
-    def run_tests(self, binary_path, test_cases_path):
-        execute_command = OJ_EXECUTE_COMMAND + ["--c", binary_path, "-d", test_cases_path]
+    def run_tests(self, binary_path):
+        execute_command = OJ_EXECUTE_COMMAND + ["--c", binary_path, "-d", self.test_cases_path]
         subprocess.call(execute_command)
 
 
