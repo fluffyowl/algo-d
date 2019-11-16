3d4e1d9c205a07d8fecebfd24802f1551958a928
fluffyowl <nebukuro09@gmail.com>
2019-11-16 19:57:14 +0900

    Issue#48: Add script for resolving dependency

---
+++ resolve.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import util
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: " + os.path.basename(__file__) + " path/to/library")
+        exit(1)
+    os.chdir(os.path.dirname(__file__))
+    path_to_library = os.path.abspath(sys.argv[1])
+    with open(path_to_library) as f:
+        source = "\n" + f.read()
+    source += "\n" + util.generate_required_content(path_to_library)
+    print (source)
+
+if __name__ == "__main__":
+    main()
